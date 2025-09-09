923a3c270d45f284dbb147d32ef8eccb20966d1a
coolerputt01 <olumideadekolu9@gmail>
2025-09-09 13:02:19 -0700

    I fixed the motion and added a platform system

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <optional>
 #include <cmath>
 
+using namespace std;
+
+const float GRAVITY = 800.f;
+const float MOVE_SPEED = 300.f; 
+const float JUMP_STRENGTH = -350.f;
 
 class Player{
     public:
-        sf::CircleShape sprite;
-        Player(float radius = 50.f) : sprite(radius){
-            sprite.setOrigin(sf::Vector2f({0.f, 0.f }));
+        sf::RectangleShape sprite;
+        sf::Vector2f velocity;
+        bool onGround = false;
+        Player(sf::Vector2f width_height = sf::Vector2f({50.f, 50.f})) : sprite(width_height){
+            sprite.setOrigin(sf::Vector2f({0.f, 0.f}));
             sprite.setFillColor(sf::Color::Green);
         }
 };
@@ -19,43 +27,59 @@ void gameLoop(Player &character, sf::RenderWindow &window){
     bool is_in_pause = false;
     bool is_finished = false;
 
-    while(is_in_game && window.isOpen()){
+    sf::Clock clock;
 
+    sf::Font font("assets/fonts/ByteBounce.ttf");
+    sf::Text text(font);
+    text.setPosition(sf::Vector2f({
+        360.f,0.f
+    }));
+    text.setString("Dash-Mates");
+    text.setCharacterSize(48);
+    text.setFillColor(sf::Color::White);
+
+    while(is_in_game && window.isOpen()){
         while (const std::optional event = window.pollEvent() )
         {
             if(event->is<sf::Event::Closed>()){
                 window.close();
             }
         }
+        float delta = clock.restart().asSeconds();
 
-        window.clear(sf::Color::White);
+        window.clear(sf::Color::Black);
         window.draw(character.sprite);
+        window.draw(text);
         window.display();
         // Listen for input.
+
+        sf::Vector2f direction = sf::Vector2f({0.f,0.f});
+
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
-            character.sprite.move(sf::Vector2f({-1.f,0.f}));
+            direction.x -= 1.f;
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){
-            character.sprite.move(sf::Vector2f({1.f,0.f}));
+            direction.x += 1.f;
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)){
-            character.sprite.move(sf::Vector2f({0.f,-1.f}));
+        
+
+        character.velocity.x = direction.x * MOVE_SPEED;
+
+        if(!character.onGround){
+            character.velocity.y += GRAVITY * delta;
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)){
-            character.sprite.move(sf::Vector2f({0.f,1.f}));
+
+        float groundY = 600.f - character.sprite.getSize().y; 
+        if(character.sprite.getPosition().y >= groundY){
+            character.sprite.setPosition({character.sprite.getPosition().x, groundY});
+            character.velocity.y = 0.f;
+            character.onGround = true;
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && character.onGround){
+                character.velocity.y = JUMP_STRENGTH;
+                character.onGround = false;
+            }
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)){
-            character.sprite.move(sf::Vector2f({(-1.f/sqrt(2.f)),(1.f/sqrt(2.f))}));
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)){
-            character.sprite.move(sf::Vector2f({(1.f/sqrt(2.f)),(1.f/sqrt(2.f))}));
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)){
-            character.sprite.move(sf::Vector2f({(-1.f/sqrt(2.f)),(-1.f/sqrt(2.f))}));
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)){
-            character.sprite.move(sf::Vector2f({(1.f/sqrt(2.f)),(-1.f/sqrt(2.f))}));
-        }
+        character.sprite.move(character.velocity * delta);
     }
 }
 int main(){
